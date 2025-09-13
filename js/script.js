42242aaac1e06ed21b773a38f554f124baeb28cb
jaber-2004 <md9292463@gmail.com>
2025-09-13 16:07:34 +0600

    js add

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Counterup start
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
+
+// Counterup end
+
+// Venobox start
+new VenoBox({
+  selector: ".my-image-links",
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: "rotating-plane",
+});
+// Venobox end
